db58d8eef1912d88e26217cb6f1528a033aa8a4a
EVKoene <emielkoene@gmail.com>
2024-07-20 11:15:57 +0200

    Add stampede

--- player/DeckCollection.gd
+++ player/DeckCollection.gd
@@ -105,7 +105,7 @@ var opponent_testing_deck: Dictionary = animal_deck
 
 var player_testing_deck: Dictionary = {
 	"Cards": {
-		cards.RHINOCEROS: 10,
+		cards.STAMPEDE: 10,
 	},
 	"StartingCards": {
 		cards.GORILLA: 2,
