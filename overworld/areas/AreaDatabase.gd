07de289ea2e4099a42289851bf6b2706d3d7d420
EVKoene <99871159+EVKoene@users.noreply.github.com>
2025-01-01 11:12:05 +0100

    Add start of passion (#99)

--- overworld/areas/AreaDatabase.gd
+++ overworld/areas/AreaDatabase.gd
@@ -1,22 +1,31 @@
 extends Node
 
 
-enum area_ids { START_OF_JOURNEY, START_OF_GROWTH }
+enum area_ids { START_OF_JOURNEY, START_OF_GROWTH, START_OF_PASSION }
 
 var areas := {
 	area_ids.START_OF_JOURNEY: {
-		"AreaName": "StartOfJourney",
+		"AreaName": "Start Of Journey",
 		"ScenePath": "res://overworld/areas/StartOfJourney.tscn",
 		"TransitionPosition": {
 			area_ids.START_OF_GROWTH: Vector2(497, 88),
+			area_ids.START_OF_PASSION: Vector2(220, -99)
 		},
 		"StartingPosition": Vector2(286, 150)
 	},
 	area_ids.START_OF_GROWTH: {
-		"AreaName": "StartOfGrowth",
+		"AreaName": "Start Of Growth",
 		"ScenePath": "res://overworld/areas/StartOfGrowth.tscn",
 		"TransitionPosition": {
 			area_ids.START_OF_JOURNEY: Vector2(86, 134),
+		},
+		"StartingPosition": Vector2(86, 134)
+	},
+	area_ids.START_OF_PASSION: {
+		"AreaName": "Start Of Passion",
+		"ScenePath": "res://overworld/areas/StartOfPassion.tscn",
+		"TransitionPosition": {
+			area_ids.START_OF_JOURNEY: Vector2(1264, 827),
 		},
 		"StartingPosition": Vector2(86, 134)
 	},
